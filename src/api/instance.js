19f03299ffee9daead030fd397ced0f90202bf44
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-09-22 04:59:49 +0900

    Feat : axios instance 추가

---
+++ src/api/instance.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_API,
+  timeout: 10000,
+})
+
+api.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error),
+)
+
+api.interceptors.response.use(
+  (response) => {
+    try {
+      return response
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  },
+  (error) => Promise.reject(error),
+)
+
+const getRequest = async (url) => {
+  const response = await api.get(url)
+  return response.data.data
+}
+
+const postRequest = async (url, data) => {
+  const response = await api.post(url, data)
+  return response
+}
+
+const putRequest = async (url, data) => {
+  const response = await api.put(url, data)
+  return response.data
+}
+
+const deleteRequest = async (url) => {
+  const response = await api.delete(url)
+  return response.data
+}
+
+export { api, postRequest, getRequest, putRequest, deleteRequest }
