e64bf5f6b0ab2aca7f7fae0f15700554dc14ac4d
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-09-23 02:11:58 +0900

    Feat : best 운동볼륨 리스트 페이지 추가

--- src/pages/exercise-volume/ExerciseList.jsx
+++ src/pages/exercise-volume/ExerciseList.jsx
@@ -1,5 +1,26 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getExerciseBest } from 'api/exercise'
+
+import { PageTitle } from 'components/common'
+import { BoardList } from 'components/list'
+
 const ExerciseList = () => {
-  return <div>ExerciseList</div>
+  // todo: 에러페이지 제작 후 isError || error일 때 해당 페이지로 랜딩
+  const { data, isLoading, isSuccess, isError, error } = useQuery({
+    queryKey: ['getExerciseBest'],
+    queryFn: () => getExerciseBest(),
+    throwOnError: (err) => console.error(err),
+  })
+
+  // todo: 무한스크롤
+  return (
+    <>
+      <PageTitle>🍜 BEST 운동볼륨</PageTitle>
+      {isLoading && <p>로딩중</p>}
+      {isSuccess && data && <BoardList type="exercise_volume" path="/exercise-volume" list={data} />}
+    </>
+  )
 }
 
 export default ExerciseList
