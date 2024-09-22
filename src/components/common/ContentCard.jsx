7b596e3acc229a1bbb32e7ef24bd477e130e6e46
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-09-22 11:49:29 +0900

    [feat] 프로젝트 세팅 및 공통 컴포넌트 추가 (#2)
    - 프로젝트 기본 설정
    - axios interceptor 추가
    - UI 공통 컴포넌트 구현

---
+++ src/components/common/ContentCard.jsx
@@ -0,0 +1,60 @@
+import { useNavigate } from 'react-router-dom'
+
+import styled from 'styled-components'
+
+import Image from './Image'
+import QuadImages from './QuadImages'
+import Button from './Button'
+
+const ContentCard = ({ type, isQuad, urlArrs, src, alt, title, desc, boardId, showBtn }) => {
+  const navigate = useNavigate()
+
+  const browserTarget = type === 'food' ? 'meal' : type === 'exercise_volume' ? 'exercise-volume' : ''
+
+  return (
+    <ContentCardWrap>
+      {isQuad ? <QuadImages urlArrs={urlArrs} /> : <Image src={src} alt={alt} />}
+      <TitleDiv onClick={browserTarget && boardId ? () => navigate(`/${browserTarget}?id=${boardId}`) : ''}>
+        <h2>{title}</h2>
+        {showBtn && <Button color="mainBlue">선택</Button>}
+      </TitleDiv>
+      <p>{desc}</p>
+    </ContentCardWrap>
+  )
+}
+
+export default ContentCard
+
+const ContentCardWrap = styled.div`
+  width: 154px;
+  & > p {
+    margin-top: 2px;
+    width: 100%;
+    font-size: 11px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`
+
+const TitleDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+  & > h2 {
+    width: calc(100% - 40px);
+    font-size: 15px;
+    font-weight: ${({ theme }) => theme.fontWeight.title};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    cursor: pointer;
+    & + button {
+      display: flex;
+      align-items: center;
+      padding: 0 4px;
+      height: 18px;
+      font-size: 11px;
+    }
+  }
+`
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,7 +1,10 @@
 import Button from './Button'
+import ContentCard from './ContentCard'
 import Footer from './Footer'
 import Layout from './Layout'
 import Navbar from './Navbar'
+import QuadImages from './QuadImages'
+import SectionTitle from './SectionTitle'
 import Sidebar from './Sidebar'
 
-export { Button, Footer, Layout, Navbar, Sidebar }
+export { Button, ContentCard, Footer, Layout, Navbar, QuadImages, SectionTitle, Sidebar }
