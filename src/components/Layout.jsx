ac4c45ec5ccef447e2abeda24554ac3c97c065d0
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-09-24 10:22:17 +0900

    Style : 모바일 기준 반응형 width, height 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -21,15 +21,17 @@ export default Layout
 
 const MaxWidthDiv = styled.div`
   position: relative;
-  margin: auto;
-  width: 360px;
+  margin: 0 auto;
+  min-width: 320px;
+  width: 100dvh;
+  max-width: 430px;
   height: 100%;
 `
 
 const WrapMain = styled.main`
-  padding: 40px 20px;
-  height: calc(100dvh - 144px);
-  overflow-y: scroll;
+  height: 100%;
+  padding: 40px 20px 100px 20px;
+  overflow-y: auto;
   box-shadow: rgba(0, 0, 0, 0.8) 0px 5px 18px -7px;
   &::-webkit-scrollbar {
     width: 2px;
