ec60b3bb351685f55e6c1b9830cae82ba4258a3c
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-10-15 23:40:34 +0900

    Feat : footer 이메일 삭제 및 구글폼 추가

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -1,4 +1,5 @@
 import iconCalc from './icon-calc.png'
+import iconForm from './icon-form.png'
 import iconGithub from './icon-github.png'
 import iconHealth from './icon-health.png'
 import iconHome from './icon-home.png'
@@ -7,4 +8,4 @@ import iconKakao from './icon-kakao.png'
 import iconMeal from './icon-meal.png'
 import iconProtein from './icon-protein.png'
 
-export { iconCalc, iconGithub, iconHealth, iconHome, iconImgError, iconKakao, iconMeal, iconProtein }
+export { iconCalc, iconForm, iconGithub, iconHealth, iconHome, iconImgError, iconKakao, iconMeal, iconProtein }
